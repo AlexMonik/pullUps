de4873a295b9134be44284e39d0412c86ce82dea
Aleksandr Sukhomlinov <aleksandr@clips4sale.com>
2022-08-14 19:21:09 +0800

    echo example

---
+++ app/main.py
@@ -0,0 +1,32 @@
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils.executor import start_webhook
+from aiogram import Bot, types
+from misc import settings
+
+bot = Bot(token=settings.TOKEN)
+dp = Dispatcher(bot)
+
+
+async def on_startup(dispatcher):
+    await bot.set_webhook(settings.WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=settings.WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=settings.WEBAPP_HOST,
+        port=settings.WEBAPP_PORT,
+    )
