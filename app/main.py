fa6e34f2ff2ebd165fb8ebde85f76fcbb23438da
Aleksandr Sukhomlinov <aleksandr@clips4sale.com>
2022-08-14 20:07:04 +0800

    add logging

--- app/main.py
+++ app/main.py
@@ -2,31 +2,41 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils.executor import start_webhook
 from aiogram import Bot, types
 from misc import settings
+from loguru import logger
+
 
 bot = Bot(token=settings.TOKEN)
 dp = Dispatcher(bot)
 
 
 async def on_startup(dispatcher):
+    logger.info('start app')
     await bot.set_webhook(settings.WEBHOOK_URL, drop_pending_updates=True)
 
 
 async def on_shutdown(dispatcher):
+    logger.info('shutdown app')
     await bot.delete_webhook()
 
 
 @dp.message_handler()
 async def echo(message: types.Message):
+    logger.debug(f'got message: {message.text}')
     await message.answer(message.text)
 
 
 if __name__ == '__main__':
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=settings.WEBHOOK_PATH,
-        skip_updates=True,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        host=settings.WEBAPP_HOST,
-        port=settings.WEBAPP_PORT,
-    )
+    try:
+        start_webhook(
+            dispatcher=dp,
+            webhook_path=settings.WEBHOOK_PATH,
+            skip_updates=True,
+            on_startup=on_startup,
+            on_shutdown=on_shutdown,
+            host=settings.WEBAPP_HOST,
+            port=settings.WEBAPP_PORT,
+        )
+    except Exception as main_exception:
+        import sys
+        logger.exception(main_exception)
+        sys.exit()
